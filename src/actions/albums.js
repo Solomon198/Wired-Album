c6382f85dfbb5fcfbb42d9302f6bddb6798ba801
Solex <solomonyunana95@gmail.com>
2022-02-07 17:41:54 +0100

    removed dummy text added for testing purpose"

--- src/actions/albums.js
+++ src/actions/albums.js
@@ -13,9 +13,6 @@ async function GetAlbums(userId=1){
 async function GetAlbum(albumId){
     try{
         const response = await axios.get(`/albums/${albumId}/photos`);
-        const responseData  = response.data;
-        responseData.push({title:'manipulating strings if funny and fund at th same time.',userId: 1,id: 11,thumbnailUrl:responseData[0].thumbnailUrl});
-
         return {success: true,data: response.data}
      }catch(e){
        return {success: false,data: `Could not fetch album Information, ${e.message}`}
